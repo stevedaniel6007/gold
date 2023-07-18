739bd028f08a7786fa50df8385f92f6efecb59b6
InvincibleInventor <invincibleinventor@gmail.com>
2023-07-18 05:42:20 +0000

    ok

---
+++ src/routes/sample/index@mapqr.tsx
@@ -0,0 +1,82 @@
+import { component$ } from "@builder.io/qwik";
+import CryptoJS from "crypto-js";
+import QRCode from "qrcode";
+import { supabase } from "~/services/firebase";
+//import QRCode from "react-qr-code";
+ 
+export async function genqr(){
+    const {data,error} =  await supabase.from('commerce').select('*')
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log('ok'+data)
+    }
+    const b=data!=null?data:[]
+    for(let i =0;i<=b.length-1;i++){
+        console.log(b[i])
+       const  encrypted = CryptoJS.AES.encrypt(String(b[i]["ADM NO"]),"thisisthebestkeyforhashingit").toString()
+    
+        if(b[i]["Hash"]==null){
+        await supabase.from('commerce').upsert({"ADM NO":b[i]["ADM NO"],"STUDENT NAME":b[i]["STUDENT NAME"],"CLASS":b[i]["CLASS"],"Hash":encrypted,})
+
+        }
+        console.log(b.length)
+
+    }
+}
+
+export async function getFiles(){
+    genqr()
+const {data} =  await supabase.from('commerce').select('*')
+console.log(data)
+const b=data!=null?data:[]
+for(let i =0;i<=b.length-1;i++){
+    console.log(b[i])
+    QRCode.toCanvas(b[i]['Hash'], function (err,canvas){
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(canvas)
+            //downloadURI(url,b[i]['ID'])
+            document.getElementById('ok')?.append(canvas)
+            canvas.classList.add('my-2')
+            canvas.classList.add('mx-2')
+            const context = canvas.getContext('2d')
+            if(context!=null)
+            {            context.font = "8pt Calibri";}
+            context?.fillText(b[i]['ROLL NO'], 20, 12);
+            context?.fillText(b[i]['ADM NO'], 20, 160);
+            downloadURI(canvas.toDataURL(),b[i]['Admission Num'])
+
+        }
+    })
+    
+}
+
+    function downloadURI(uri:any, name:any) {
+
+        const link = document.createElement("a");
+        link.download = name;
+        link.href = uri;
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+}
+
+
+export default component$(()=>{
+
+    return(
+        <div class="flex h-full w-full items-center content-center flex-col">
+            <div id="ok" class="grid  md:grid-cols-2  grid-cols-1 lg:grid-cols-8">
+
+            </div>
+<button class="subm"  onClick$={()=>getFiles()}>Get All QRs</button>
+   
+   </div>)
+
+})
